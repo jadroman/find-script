51ed11d547050cd6ef86a55e050a4c3729e50756
jadroman <jadrangrba@gmail.com>
2014-06-03 14:38:49 +0200

    započeta implementacija error middleware-a

--- app.js
+++ app.js
@@ -10,12 +10,17 @@ var configDB = require('./config/database.js');
 mongoose.connect(configDB.url);
 
 
+
+
 app.configure(function () {
 	app.set('port', process.env.PORT || 3000);
 
 	app.use(express.favicon());
 	app.use(express.cookieParser());
 	app.use(express.bodyParser({ keepExtensions: true }));
+	
+	app.use(badMiddleware)
+	
 	app.use(express.methodOverride());
 	app.use(express.session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 	app.use(flash()); // use connect-flash for flash messages stored in session
@@ -29,9 +34,11 @@ app.configure(function () {
 	app.use("/js", express.static(__dirname + '/js'));
 	//app.use("/js/jquery-ui/js", express.static(__dirname + '/js/jquery-ui/js'));
 
+	
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'ejs');
 	
+	app.use(logError)
 		
 	app.locals.pretty = true;
 });
@@ -44,3 +51,17 @@ require('./routes.js')(app, passport);
 http.createServer(app).listen(app.get('port'), function () {
 	console.log("Express server listening on port " + app.get('port'));
 });
+
+
+
+function badMiddleware(req, res, next) {
+	
+  next(new Error('Bad middleware makes error'));
+}
+
+function logError(err, req, res, next) {
+  
+	console.log("#################################################################");
+	res.send(500, 'Something broke!');
+  
+}
